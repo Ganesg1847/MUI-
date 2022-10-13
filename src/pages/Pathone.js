2534bd71f0f8dc1e937380cd1bcf4270d795fafc
Siddemma <siddemma507@gmail.com>
2022-10-13 15:39:31 +0530

    static

--- src/pages/Pathone.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Pathone() {
-  return (
-    <div>
-      <h1>Pathone</h1>
-    </div>
-  )
-}
--- src/pages/Pathsecond.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Pathsecond() {
-  return (
-    <div>
-      <h1>Pathsecond</h1>
-    </div>
-  )
-}
